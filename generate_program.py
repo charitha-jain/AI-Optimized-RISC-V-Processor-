0c09883935a213c44bbefb4b4ccafc86cc004cb1
Charitha Jain <jaincharitha3124@gmail.com>
2025-07-24 20:37:14 +0530

    seg folders

--- generate_program.py
+++
@@ -1,4 +0,0 @@
-# generate_program.py
-with open("program.hex", "w") as file:
-    for i in range(1024):  # Generate exactly 1024 words
-        file.write(f"{i:08X}\n")  # Write the value as 8-digit hex, padded
